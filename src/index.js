fb19003ac7e29c9f865e655204d1e21628250032
sarahmcdougall <smcdougall@mitre.org>
2021-08-20 11:14:09 -0400

    connected to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-const { initialize, loggers } = require('@asymmetrik/node-fhir-server-core');
+const { initialize, loggers, constants } = require('@asymmetrik/node-fhir-server-core');
+const mongoUtil = require('./util/mongo');
+const { VERSIONS } = constants;
 
-let config = {};
+let config = {
+  profiles: {
+    patient: {
+      service: './src/services/patient.service.js',
+      versions: [VERSIONS['4_0_0']]
+    }
+  }
+};
 let server = initialize(config);
 let logger = loggers.get('default');
 
-server.listen(3000, () => {
+server.listen(3000, async () => {
   logger.info('Starting the FHIR Server at localhost:3000');
+  await mongoUtil.client.connect();
+  logger.info('Connected to database');
 });
