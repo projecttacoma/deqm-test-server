5c350f9fbdcd0aa6f51b3eb0a38a68a5246233cd
Chris Hossenlopp <hossenlopp@mitre.org>
2021-08-26 16:09:07 -0400

    added exception service for Measure

--- src/util/config.js
+++ src/util/config.js
@@ -2,6 +2,7 @@ const { constants } = require('@asymmetrik/node-fhir-server-core');
 const { VERSIONS } = constants;
 const supportedResources = require('./supportedResources');
 const { buildServiceModule } = require('../services/base.service');
+const path = require('path');
 
 /**
  * Build configuration object to pass to the Asymmetrik core FHIR server
@@ -14,6 +15,12 @@ const buildConfig = () => {
   };
   supportedResources.forEach(resourceType => {
     switch (resourceType) {
+      case 'Measure':
+        config.profiles['Measure'] = {
+          service: path.resolve('src', 'services', 'measure.service.js'),
+          versions: [VERSIONS['4_0_0']]
+        };
+        break;
       default:
         config.profiles[resourceType] = {
           service: buildServiceModule(resourceType),
