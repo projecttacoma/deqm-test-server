6528b7fd7c6f794d94b8c1a9f9733e188e95c48f
sarahmcdougall <smcdougall@mitre.org>
2021-09-01 15:17:07 -0400

    used express and body parser to support measure bundles

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const { loggers } = require('@asymmetrik/node-fhir-server-core');
 const mongoUtil = require('./util/mongo');
 const { buildConfig } = require('./util/config');
 const { initialize } = require('./server/server');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json({ limit: '50mb', type: 'application/json+fhir' }));
 
 let config = buildConfig();
-let server = initialize(config);
+let server = initialize(config, app);
 let logger = loggers.get('default');
 
 server.listen(3000, async () => {
