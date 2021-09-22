bcfb189dd85a1ad21ae20b43f43989ff1bd66ce0
mriley <mriley@mitre.org>
2021-09-22 15:03:50 -0400

    Changes after debugging

--- test/measure.service.test.js
+++ test/measure.service.test.js
@@ -1,29 +1,39 @@
 //{BASE_URL}/4_0_0/Measure/{id}
 // for measure testing this the base url
+require('../src/util/dbconfig');
+const testMeasure = require('./fixtures/testMeasure.json');
+const testLibrary = require('./fixtures/testLibrary.json');
+const testPatient = require('./fixtures/testPatient.json');
+const { testSetup, cleanUpDb } = require('./populateTestData');
+const supertest = require('supertest');
+const app = require('../src/index');
+const route = require('../src/index');
+const { buildConfig } = require('../src/util/config');
+const { initialize } = require('../src/server/server');
+const config = buildConfig();
+const server = initialize(config);
+
 const BASE_URL = 'localhost:3000/4_0_0/Measure/testMeasure';
+const trailing_URL = ' /4_0_0/Measure/testMeasure';
 const data = {
   headers: 'application/json+fhir',
   title: 'test1',
   id: 'testMeasure'
 };
 
-const { testSetup, cleanUpDb } = require('./populateTestData');
-const supertest = require('supertest');
-const app = require('../src/index');
-//const DEQMTestServer = require('../src/server/server');
-//const apptest = supertest(http.createServer(DEQMTestServer.callback()));
- describe('measure.service', () => {
-  /**populateMeasures */
-  beforeEach(() => {
-    testSetup('testMeasure.json', 'testPatient.json', 'testLibrary.json');
+describe('measure.service', () => {
+  beforeAll(async () => {
+    await testSetup(testMeasure, testPatient, testLibrary);
   });
-  /* afterEach(done => {
-    cleanUpDb()
-  });*/
-
+  afterAll(async () => {
+    await cleanUpDb();
+  });
+  test('test', async () => {
+    await supertest(server.app).get('/4_0_0/metadata').expect(200);
+  });
   describe('create', () => {
     test('test create with correct headers', async () => {
-      await supertest(app)
+      await supertest(server.app)
         .post(BASE_URL)
         .send(data)
         .expect(200)
@@ -39,13 +49,17 @@ const app = require('../src/index');
           expect(post.title).toBe(data.title);
           expect(post.content).toBe(data.content);
         });
+      //console.log(app.server);
+      //  const response = await request(route).post(BASE_URL).send(data).expect(200);
+      //expect(response.body._id.toEqual(testLibrary.id));
     });
   });
   describe('searchById', () => {
     //* result of sending a GET request to {BASE_URL}/4_0_0/Measure/{id}
-    test('test searchById with correctHeaders and  the id should be in database', () => {
-      supertest(app)
-        .get(BASE_URL)
+    test('test searchById with correctHeaders and  the id should be in database', async () => {
+      await supertest(server.app)
+        //.get(BASE_URL)
+        .get(trailing_URL)
         .send(data)
         .expect(200)
         .then(async response => {
@@ -73,7 +87,7 @@ const app = require('../src/index');
     };
     //*a put request*/
     test('test update with correctHeaders and  the id is in database', () => {
-      supertest(app)
+      supertest(server.app)
         .put(BASE_URL)
         .send(updateData)
         .expect(200)
@@ -92,7 +106,7 @@ const app = require('../src/index');
   });
   describe('remove', () => {
     test('removing the measure from the database when the measure is indeed present', () => {
-      supertest(app)
+      supertest(server.app)
         .delete(BASE_URL)
         .send(data)
         .expect(200)
--- test/populateTestData.js
+++ test/populateTestData.js
@@ -1,17 +1,19 @@
-const { db } = require('../src/util/mongo');
-require('dotenv').config({ path: './.env.test' });
-
+const { db, client } = require('../src/util/mongo');
 const createTestResource = async (data, resourceType) => {
   const collection = db.collection(resourceType);
   await collection.insertOne(data);
   return { id: data.id };
 };
 //clean up db after test
-function cleanUpDb() {}
-
-function testSetup(pathToFiles, pathToPatients, pathToTestLibrary) {
-  createTestResource(pathToFiles, 'Measure');
-  createTestResource(pathToPatients, 'Patient');
-  createTestResource(pathToTestLibrary, 'Library');
+async function cleanUpDb() {
+  await db.dropDatabase();
+  await client.close();
 }
+
+const testSetup = async (pathToFiles, pathToPatients, pathToTestLibrary) => {
+  await client.connect();
+  await createTestResource(pathToFiles, 'Measure');
+  await createTestResource(pathToPatients, 'Patient');
+  await createTestResource(pathToTestLibrary, 'Library');
+};
 module.exports = { testSetup, cleanUpDb };
