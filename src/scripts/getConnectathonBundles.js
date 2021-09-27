47b3b5c83fae2b1c1f8cac0259cc2a628532d3b7
sarahmcdougall <smcdougall@mitre.org>
2021-09-27 14:58:56 -0400

    completed cthon bundle upload

--- src/scripts/getConnectathonBundles.js
+++ src/scripts/getConnectathonBundles.js
@@ -1,34 +1,68 @@
 const fs = require('fs');
 const path = require('path');
+const mongoUtil = require('../util/mongo');
+const { createResource } = require('../util/mongo.controller');
 
 const connectathonPath = path.resolve(path.join(__dirname, '../../connectathon/fhir401/bundles/measure/'));
+// bundles that are not the latest available version
+const IGNORED_BUNDLES = ['EXM347-3.2.000-bundle.json'];
+// files containing EXM bundles of interest from connectathon repo
+const bundleFiles = [];
 
 /**
- * Sends POST requests to the transaction bundle endpoint for every measure in the
- * connectathon repo.
+ * Retrieves EXM bundle files from the connectathon repo using a regular expression.
+ * Uses recursion to parse through all available subdirectories.
+ * @param {string} directory - directory path to start at
+ * @returns array of string paths that represent the bundle files of interest
+ */
+const getBundleFiles = directory => {
+  const fileNameRegExp = new RegExp(/(^EXM.*.json$)/);
+  const filesInDirectory = fs.readdirSync(directory);
+  filesInDirectory.forEach(file => {
+    const absolute = path.join(directory, file);
+    if (fs.statSync(absolute).isDirectory()) {
+      getBundleFiles(absolute);
+    } else if (fileNameRegExp.test(file) && !IGNORED_BUNDLES.includes(file)) {
+      bundleFiles.push(absolute);
+    }
+  });
+  return bundleFiles;
+};
+
+/**
+ * Uploads all the resources from the connectathon measure bundles into the
+ * database.
  *
  * TODO: Currently configured for fhir401 connectathon measure bundles,
  * but may want to expand to other measure bundle providers in the future.
  */
 async function main() {
-  console.log('hey');
-  console.log(connectathonPath);
-  // first try to just parse through the repo and get the measure bundle for each bundle
-  fs.readdir(connectathonPath, function (err, subDirs) {
-    if (err) {
-      console.error('could not access directories within connectathon repo');
-    }
+  const bundleFiles = getBundleFiles(connectathonPath);
 
-    subDirs.forEach(dir => {
-      // this works
-      console.log(dir);
-    });
+  await mongoUtil.client.connect();
+  console.log('Connected successfully to server');
+
+  const bundlePromises = bundleFiles.map(async filePath => {
+    // read each EXM bundle file
+    const data = fs.readFileSync(filePath, 'utf8');
+    if (data) {
+      const bundle = await JSON.parse(data);
+      // retrieve each resource and insert into database
+      const uploads = bundle.entry.map(async res => {
+        try {
+          await createResource(res.resource, res.resource.resourceType);
+        } catch (e) {
+          console.log(e.message);
+        }
+      });
+      await Promise.all(uploads);
+    }
   });
+  await Promise.all(bundlePromises);
+  return 'Connectathon bundle entries uploaded.';
 }
 
 main()
+  .then(console.log)
   .catch(console.error)
-  .catch(e => {
-    console.error(e);
-  });
-//.finally(() => mongoUtil.client.close());
+  .finally(async () => await mongoUtil.client.close());
