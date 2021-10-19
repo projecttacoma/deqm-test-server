23bcccc8726b94b15762a983df09120f2b423e1c
Sarah McDougall <87094479+sarahmcdougall@users.noreply.github.com>
2021-10-19 09:10:12 -0400

    Dynamic Export URL Handling (#36)
    * updated package.json to bring in bulk-data-utilities
    
    * added bulk-data-utilities as a dependency
    
    * updated bdu dependency
    
    * added unit tests
    
    * removed unnecessary console.logs
    
    * removed unused test cases
    
    * updated test name
    
    * fixed test json formatting, updated package.json for bdu
    
    * updated bdu dependency in package.json

--- src/services/measure.service.js
+++ src/services/measure.service.js
@@ -4,7 +4,7 @@ const { Calculator } = require('fqm-execution');
 const { baseCreate, baseSearchById, baseRemove, baseUpdate, baseSearch } = require('./base.service');
 const { createTransactionBundleClass } = require('../resources/transactionBundle');
 const { uploadTransactionBundle } = require('./bundle.service');
-const { validateEvalMeasureParams } = require('../util/measureOperationsUtils');
+const { validateEvalMeasureParams, retrieveExportURL } = require('../util/measureOperationsUtils');
 const {
   getMeasureBundleFromId,
   getPatientDataBundle,
@@ -123,7 +123,7 @@ const submitData = async (args, { req }) => {
   const parameters = req.body.parameter;
   // Ensure exactly 1 measureReport is in parameters
   const numMeasureReportsInput = parameters.filter(
-    param => param.name === 'measureReport' || param.resource.resourceType === 'MeasureReport'
+    param => param.name === 'measureReport' || param.resource?.resourceType === 'MeasureReport'
   ).length;
   if (numMeasureReportsInput !== 1) {
     throw new ServerError(null, {
@@ -170,17 +170,13 @@ const bulkImport = async (args, { req }) => {
   // id of inserted client
   const clientEntry = await addPendingBulkImportRequest();
   const res = req.res;
-  res.status(202);
+
   res.setHeader('Content-Location', `${args.base_version}/bulkstatus/${clientEntry}`);
-  //Temporary solution. Asymmetrik automatically rewrites this to a 200.
-  //Rewriting the res.status method prevents the code from being overwritten.
-  //TODO: change this once we fork asymmetrik
-  res.status = () => res;
 
   // use measure ID and export server location to map to data-requirements
   let measureId;
   let measureBundle;
-
+  const parameters = req.body.parameter;
   // case 1: request is in Measure/<id>/$submit-data format
   if (req.params.id) {
     measureId = req.params.id;
@@ -188,8 +184,7 @@ const bulkImport = async (args, { req }) => {
   }
   // case 2: request is in Measure/$submit-data format
   else {
-    const parameters = req.body.parameter;
-    const measureReport = parameters.filter(param => param.resource.resourceType === 'MeasureReport')[0];
+    const measureReport = parameters.filter(param => param.resource?.resourceType === 'MeasureReport')[0];
     // get measure resource from db that matches measure param since no id is present in request
     const query = getQueryFromReference(measureReport.resource.measure);
     const measureResource = await findOneResourceWithQuery(query, 'Measure');
@@ -210,10 +205,29 @@ const bulkImport = async (args, { req }) => {
       ]
     });
   }
-
   // retrieve data requirements
-  const results = await RequirementsQuery.retrieveBulkDataFromMeasureBundle(measureBundle);
-  return results;
+  const exportURL = retrieveExportURL(parameters);
+  const bulkDataResults = await RequirementsQuery.retrieveBulkDataFromMeasureBundle(measureBundle, exportURL);
+  if (!bulkDataResults.output && bulkDataResults.error) {
+    throw new ServerError(null, {
+      statusCode: 400,
+      issue: [
+        {
+          severity: 'error',
+          code: 'BadRequest',
+          details: {
+            text: `Received AxiosError: ${bulkDataResults.error}`
+          }
+        }
+      ]
+    });
+  }
+  res.status(202);
+  //Temporary solution. Asymmetrik automatically rewrites this to a 200.
+  //Rewriting the res.status method prevents the code from being overwritten.
+  //TODO: change this once we fork asymmetrik
+  res.status = () => res;
+  return bulkDataResults.output;
 };
 
 /**
--- test/measure.service.test.js
+++ test/measure.service.test.js
@@ -2,6 +2,9 @@ require('../src/util/dbconfig');
 const testMeasure = require('./fixtures/testMeasure.json');
 const testLibrary = require('./fixtures/testLibrary.json');
 const testPatient = require('./fixtures/testPatient.json');
+const testParam = require('./fixtures/parametersObjs/paramNoExport.json');
+const testParamTwoExports = require('./fixtures/parametersObjs/paramTwoExports.json');
+const testParamNoValString = require('./fixtures/parametersObjs/paramNoValueString.json');
 const { testSetup, cleanUpDb } = require('./populateTestData');
 const supertest = require('supertest');
 const { buildConfig } = require('../src/util/config');
@@ -9,55 +12,88 @@ const { initialize } = require('../src/server/server');
 const config = buildConfig();
 const server = initialize(config);
 const updateMeasure = { id: 'testMeasure', name: 'anUpdate' };
-describe('measure.service', () => {
+describe('measure.service CRUD operations', () => {
   beforeAll(async () => {
     await testSetup(testMeasure, testPatient, testLibrary);
   });
 
-  describe('create', () => {
-    test('test create with correct headers', async () => {
-      await supertest(server.app)
-        .post('/4_0_0/Measure')
-        .send(testMeasure)
-        .set('Accept', 'application/json+fhir')
-        .set('content-type', 'application/json+fhir')
-        .expect(201)
-        .then(response => {
-          expect(response.headers.location).toBeDefined();
-        });
-    });
+  test('test create with correct headers', async () => {
+    await supertest(server.app)
+      .post('/4_0_0/Measure')
+      .send(testMeasure)
+      .set('Accept', 'application/json+fhir')
+      .set('content-type', 'application/json+fhir')
+      .expect(201)
+      .then(response => {
+        expect(response.headers.location).toBeDefined();
+      });
   });
 
-  describe('searchById', () => {
-    test('test searchById with correctHeaders and the id should be in database', async () => {
-      await supertest(server.app)
-        .get('/4_0_0/Measure/testMeasure')
-        .set('Accept', 'application/json+fhir')
-        .set('content-type', 'application/json+fhir')
-        .expect(200)
-        .then(async response => {
-          expect(response.body.id).toEqual(testMeasure.id);
-        });
-    });
+  test('test searchById with correctHeaders and the id should be in database', async () => {
+    await supertest(server.app)
+      .get('/4_0_0/Measure/testMeasure')
+      .set('Accept', 'application/json+fhir')
+      .set('content-type', 'application/json+fhir')
+      .expect(200)
+      .then(async response => {
+        expect(response.body.id).toEqual(testMeasure.id);
+      });
   });
-  describe('update', () => {
-    test('test update with correctHeaders and the id is in database', async () => {
-      await supertest(server.app)
-        .put('/4_0_0/Measure/testMeasure')
-        .send(updateMeasure)
-        .set('Accept', 'application/json+fhir')
-        .set('content-type', 'application/json+fhir')
-        .expect(200)
-        .then(async response => {
-          // Check the response
-          expect(response.headers.location).toBeDefined();
-        });
-    });
+
+  test('test update with correctHeaders and the id is in database', async () => {
+    await supertest(server.app)
+      .put('/4_0_0/Measure/testMeasure')
+      .send(updateMeasure)
+      .set('Accept', 'application/json+fhir')
+      .set('content-type', 'application/json+fhir')
+      .expect(200)
+      .then(async response => {
+        // Check the response
+        expect(response.headers.location).toBeDefined();
+      });
   });
-  describe('remove', () => {
-    test('removing the measure from the database when the measure is indeed present', async () => {
-      await supertest(server.app).delete('/4_0_0/Measure/testMeasure').expect(204);
-    });
+
+  test('removing the measure from the database when the measure is indeed present', async () => {
+    await supertest(server.app).delete('/4_0_0/Measure/testMeasure').expect(204);
+  });
+
+  afterAll(async () => {
+    await cleanUpDb();
+  });
+});
+
+describe('bulkImport with exportURL', () => {
+  beforeAll(async () => {
+    await testSetup(testMeasure, testPatient, testLibrary);
+  });
+  test('FHIR Parameters object is missing export URL', async () => {
+    await supertest(server.app)
+      .post('/4_0_0/Measure/$submit-data')
+      .send(testParam)
+      .set('Accept', 'application/json+fhir')
+      .set('content-type', 'application/json+fhir')
+      .set('prefer', 'respond-async')
+      .expect(400);
+  });
+
+  test('FHIR Parameters object has two export URLs', async () => {
+    await supertest(server.app)
+      .post('/4_0_0/Measure/$submit-data')
+      .send(testParamTwoExports)
+      .set('Accept', 'application/json+fhir')
+      .set('content-type', 'application/json+fhir')
+      .set('prefer', 'respond-async')
+      .expect(400);
+  });
+
+  test('FHIR Parameters object is missing valueString for export URL', async () => {
+    await supertest(server.app)
+      .post('/4_0_0/Measure/$submit-data')
+      .send(testParamNoValString)
+      .set('Accept', 'application/json+fhir')
+      .set('content-type', 'application/json+fhir')
+      .set('prefer', 'respond-async')
+      .expect(400);
   });
 
   afterAll(async () => {
