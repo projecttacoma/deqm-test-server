8d5f3c60af8cbeb42f53d0fc1c121d0c511f0bee
natjoe4 <njjones@mitre.org>
2021-12-10 13:02:23 -0500

    added jsdoc

--- src/util/baseUtils.js
+++ src/util/baseUtils.js
@@ -1,6 +1,11 @@
 const { ServerError } = require('@projecttacoma/node-fhir-server-core');
 const supportedResources = require('../server/supportedResources');
 
+/**
+ * Determines whether the passed in resourceType is one supported by our server.
+ * Throws an error if not
+ * @param {string} resourceType A string representing a FHIR resource type
+ */
 function checkSupportedResource(resourceType) {
   if (!supportedResources.includes(resourceType)) {
     throw new ServerError(null, {
