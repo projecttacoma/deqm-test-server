b860d66770fc00b7515810d0b9f1ec03cc410453
sarahmcdougall <smcdougall@mitre.org>
2021-12-17 15:25:55 -0500

    misc changes

--- src/util/patientUtils.js
+++ src/util/patientUtils.js
@@ -8,12 +8,17 @@ const { mapResourcesToCollectionBundle, mapArrayToSearchSetBundle } = require('.
 
 /**
 <<<<<<< HEAD
+<<<<<<< HEAD
  * Wrapper function to get patient data and data
  * requirements for given patient id and map the resources to a collection bundle.
 =======
  * Wrapper function to get patient data for a given patient id and its data
  * requirements and map the resources to a collection bundle.
 >>>>>>> 75d7893 (created patientUtils from bundleUtils)
+=======
+ * Wrapper function to get patient data and data
+ * requirements for given patient id and map the resources to a collection bundle.
+>>>>>>> a878b42 (misc changes)
  * @param {string} patientId patient ID of interest
  * @param {Array} dataRequirements data requirements array obtained from fqm execution
  * @returns {Object} patient bundle as a collection bundle
@@ -28,34 +33,20 @@ async function getPatientDataCollectionBundle(patientId, dataRequirements) {
  * the resources to a searchset bundle (used for Patient/$everything when
  * we are not concerned with a specific measure)
  * @param {string} patientId patient ID of interest
-<<<<<<< HEAD
- * @param {string} base_version base version froma rgs passed in through client request
+ * @param {string} base_version base version from args passed in through client request
  * @param {string} host host specified in request headers
  * @returns {Object} patient bundle as a searchset bundle
  */
 async function getPatientDataSearchSetBundle(patientId, base_version, host) {
   const data = await getPatientData(patientId);
   return mapArrayToSearchSetBundle(_.flattenDeep(data), base_version, host);
-=======
- * @param {Object} args passed in arguments
- * @param {String} host host specified in request headers
- * @returns {Object} patient bundle as a searchset bundle
- */
-async function getPatientDataSearchSetBundle(patientId, args, host) {
-  const data = await getPatientData(patientId);
-  return mapArrayToSearchSetBundle(_.flattenDeep(data), args, host);
->>>>>>> 75d7893 (created patientUtils from bundleUtils)
 }
 
 /**
  * Assemble the patient bundle to be used in our operations from fqm execution
  * @param {string} patientId patient ID of interest
  * @param {Array} dataRequirements data requirements array obtained from fqm execution,
-<<<<<<< HEAD
  * used when we are concerned with a specific measure. Otherwise, undefined.
-=======
- * used when we are concerned with a specific measure. Otherwise undefined
->>>>>>> 75d7893 (created patientUtils from bundleUtils)
  * @returns {Array} array of resources
  */
 async function getPatientData(patientId, dataRequirements) {
