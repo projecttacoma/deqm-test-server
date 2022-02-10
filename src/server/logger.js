4870ab46e77849cd32fd61f9ceb959ba7504896a
natjoe4 <35576465+natjoe4@users.noreply.github.com>
2022-02-10 13:43:10 -0500

    Apply suggestions from code review
    Co-authored-by: Matthew Gramigna <mgramigna@mitre.org>

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,11 +1,11 @@
 const { loggers, format, transports } = require('winston');
 
-const LOGGER_NAME = 'cli';
+const LOGGER_NAME = 'server-logger';
 const logFormat = format.printf(({ level, message, timestamp }) => `${timestamp} [${level}]: ${message}`);
 
 if (!loggers.has(LOGGER_NAME)) {
   loggers.add(LOGGER_NAME, {
-    level: process.env.NODE_ENV === 'debug' ? 'debug' : 'info',
+    level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
     format: format.combine(format.colorize(), format.timestamp({ format: 'HH:mm:ss.SS' }), format.align(), logFormat),
     transports: [new transports.Console({})]
   });
