0ebadbbd77753d64584e812e527adc2de4e6d913
Chris Hossenlopp <hossenlopp@mitre.org>
2022-04-08 13:55:48 -0400

    removed unused FHIR Client based execution worker code

--- src/server/execWorker.js
+++ src/server/execWorker.js
@@ -1,5 +1,4 @@
 const Queue = require('bee-queue');
-const { AsyncPatientSource } = require('cql-exec-fhir');
 const { Calculator } = require('fqm-execution');
 const logger = require('./logger');
 const mongoUtil = require('../database/connection');
@@ -14,12 +13,6 @@ const execQueue = new Queue('exec', {
   },
   removeOnSuccess: true
 });
-
-/**
- * Flag to run directly connecting to mongo for patient data. If false will use FHIR server interactions for fetching
- * patient data.
- */
-const MONGO_PATIENTS = true;
 
 // Hold measure bundles and dataRequirements for quick reuse indexed by id-MPstart-MPend with { lastUsed, bundle, dataReq }
 const measureBundleCache = {};
@@ -47,46 +40,28 @@ async function getMeasureBundle(measureId, periodStart, periodEnd) {
   const measureBundle = await getMeasureBundleFromId(measureId);
   measureBundleCache[cacheLabel] = {
     lastUsed: Date.now(),
-    bundle: measureBundle
-  };
-  if (MONGO_PATIENTS) {
-    measureBundleCache[cacheLabel].dataReq = await Calculator.calculateDataRequirements(measureBundle, {
+    bundle: measureBundle,
+    dataReq: await Calculator.calculateDataRequirements(measureBundle, {
       measurementPeriodStart: periodStart,
       measurementPeriodEnd: periodEnd
-    });
-  }
+    })
+  };
   return measureBundleCache[cacheLabel];
 }
 
 execQueue.process(async job => {
   logger.info(`exec-worker-${process.pid}: Execution Job Received!`);
   const measureBundle = await getMeasureBundle(job.data.measureId, job.data.periodStart, job.data.periodEnd);
-  let results;
-  if (MONGO_PATIENTS) {
-    logger.info(`exec-worker-${process.pid}: Executing with reaching to mongo for patient bundles.`);
-    let patientBundles = job.data.patientIds.map(async id => {
-      return getPatientDataCollectionBundle(id, measureBundle.dataReq.results.dataRequirement);
-    });
-    patientBundles = await Promise.all(patientBundles);
-    results = await Calculator.calculate(measureBundle.bundle, patientBundles, {
-      verboseCalculationResults: false,
-      measurementPeriodStart: job.data.periodStart,
-      measurementPeriodEnd: job.data.periodEnd
-    });
-  } else {
-    logger.info(`exec-worker-${process.pid}: Executing with reaching to FHIR server for patient data.`);
-    const patientSource = AsyncPatientSource.FHIRv401(
-      `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/4_0_1/`
-    );
-    patientSource.loadPatientIds(job.data.patientIds);
-    results = await Calculator.calculate(measureBundle.bundle, [], {
-      verboseCalculationResults: false,
-      patientSource: patientSource,
-      measurementPeriodStart: job.data.periodStart,
-      measurementPeriodEnd: job.data.periodEnd
-    });
-  }
-  return results;
+
+  let patientBundles = job.data.patientIds.map(async id => {
+    return getPatientDataCollectionBundle(id, measureBundle.dataReq.results.dataRequirement);
+  });
+  patientBundles = await Promise.all(patientBundles);
+  return await Calculator.calculate(measureBundle.bundle, patientBundles, {
+    verboseCalculationResults: false,
+    measurementPeriodStart: job.data.periodStart,
+    measurementPeriodEnd: job.data.periodEnd
+  });
 });
 
 module.exports = execQueue;
