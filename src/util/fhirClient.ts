58e6b5dcb46ad54419046d30fc62bcd445f36d0a
Dylan Hall <dehall@mitre.org>
2026-08-28 14:29:49 -0400

    introduce TokenManager and FHIRClient

---
+++ src/util/fhirClient.ts
@@ -0,0 +1,61 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import TokenManager from './tokenManager';
+
+// SMART-aware client for outgoing FHIR interactions
+export default class FHIRClient {
+  private readonly http: AxiosInstance;
+
+  constructor(private readonly baseUrl: string) {
+    this.http = axios.create({
+      baseURL: baseUrl.replace(/\/+$/, ''),
+      headers: {
+        Accept: 'application/fhir+json'
+      }
+    });
+  }
+
+  async get<T>(path: string, config: AxiosRequestConfig = {}): Promise<T> {
+    return this.request<T>({ ...config, method: 'GET', url: path });
+  }
+
+  async post<T>(path: string, body: unknown, config: AxiosRequestConfig = {}): Promise<T> {
+    return this.request<T>({
+      ...config,
+      method: 'POST',
+      url: path,
+      data: body,
+      headers: {
+        'Content-Type': 'application/fhir+json',
+        ...config.headers
+      }
+    });
+  }
+
+  private async request<T>(config: AxiosRequestConfig, retried = false): Promise<T> {
+    try {
+      const token = await TokenManager.getToken(this.baseUrl);
+
+      const headers = { ...config.headers };
+      if (token) {
+        headers['Authorization'] = token.bearerToken;
+        console.log(`making request using bearer token ${token.bearerToken}`);
+      } else {
+        console.log('making request without bearer token');
+      }
+
+      const response = await this.http.request<T>({
+        ...config,
+        headers
+      });
+
+      return response.data;
+    } catch (error) {
+      if (!retried && axios.isAxiosError(error) && error.response?.status === 401) {
+        // try again once if we got an error 401, maybe the token just expired
+        TokenManager.invalidate(this.baseUrl);
+        return this.request<T>(config, true);
+      }
+      throw error;
+    }
+  }
+}
--- src/util/measureUtils.ts
+++ src/util/measureUtils.ts
@@ -1,10 +1,9 @@
-import axios from 'axios';
 import { CalculationOptions, Calculator, DRCalculationOutput } from 'fqm-execution';
 import _ from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
 import { findResourceById, findResourceIdsWithQuery } from '../database/dbOperations';
 import { BadRequestError, ResourceNotFoundError } from './errorUtils';
-import { generateJWT, getAccessToken } from './client';
+import FHIRClient from './fhirClient';
 
 const { uploadResourcesFromBundle } = require('../services/bundle.service');
 
@@ -105,41 +104,17 @@ export async function pullResourceReferences(
   );
   const serverUrl = `${process.env.BASE_URL}/${baseVersion}`;
 
-  // We need to see if the dataEndpoint is the Epic FHIR Sandbox and if so, we want to authenticate with it
-  // store client_id, aud, etc. in environment variables for now, but they will eventually go in the schema file
-  const client_id = process.env.CLIENT_ID;
-  const token_endpoint_url = process.env.TOKEN_ENDPOINT_URL;
-
-  if (!client_id) {
-    throw new Error('CLIENT_ID is not configured');
-  }
-
-  if (!token_endpoint_url) {
-    throw new Error('TOKEN_ENDPOINT_URL is not configured');
-  }
-
-  // generate the signed JWT first
-  // this is seemingly being properly generated at the moment, you can
-  // run the client.ts file on its own to print it out
-  const jwt = await generateJWT(client_id, token_endpoint_url);
-
-  // get the bearer token
-  // this is not working right now, started to debug but didn't have time
-  const accessToken = await getAccessToken(token_endpoint_url, jwt);
-
-  console.log(accessToken);
+  const fhirClient = new FHIRClient(dataEndpoint.address);
 
   // Track an array of references for the resources returned from each query
   const resourceReferenceArrays = await Promise.all(
     queries.map(async query => {
-      const response = await axios.get(query, {
+      const bundle = await fhirClient.get<fhir4.Bundle>(query, {
         headers: {
-          Authorization: `Bearer ${accessToken}`,
           Accept: 'application/fhir+json'
         }
       });
 
-      const bundle = response.data;
       if (bundle.entry) {
         const originalReferences = bundle.entry?.map((e: fhir4.BundleEntry) =>
           e.resource?.resourceType && e.resource?.id ? `${e.resource.resourceType}/${e.resource.id}` : null
@@ -148,13 +123,13 @@ export async function pullResourceReferences(
         const results = await uploadResourcesFromBundle(bundle.entry, baseVersion);
         // Get new ids
         const references = originalReferences
-          .map((refString: string, i: number) => {
+          .map((refString: string | null, i: number): fhir4.Reference | null => {
             if (!refString) return null;
             // Note: newRef may be an operation outcome if there are issues uploading the resource. Leaving this behavior as is for now.
             const newRef =
               results[i].resource?.resourceType && results[i].resource?.id
                 ? `${results[i].resource.resourceType}/${results[i].resource.id}`
-                : null;
+                : undefined;
             return {
               reference: refString,
               identifier: {
@@ -163,7 +138,7 @@ export async function pullResourceReferences(
               }
             };
           })
-          .filter(Boolean);
+          .filter((r): r is fhir4.Reference => r !== null);
         return references;
       }
       return [];
